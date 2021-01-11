070ffd2277119a8a64ffa60fd515c601e388cd89
Dhruvadeep Malakar <57034827+dhruvadeep@users.noreply.github.com>
2021-01-11 14:35:19 +0530

    Add files via upload

---
+++ hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+  // This is a comment
+  print("Hello");
+}
+
+//Note
+// void is the return type of the function.
+//    This means that when the function executes all the code inside,
+//    it should return this value.
+// main() => he main() function can be used only once in a program.
+//    It is responsible for all types of execution such as user-defined statements, functions,
+//    and libraries function.
